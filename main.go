bb2ca8504e4cbba5acee3c140ac704f98e5f6c0a
jaime <jaime@jaimes-MacBook-Pro.local>
2022-02-07 10:19:58 -0600

    adding getPost

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type post struct {
 	ID     int    `json:"ID"`
 	Title  string `json:"Title"`
@@ -16,7 +25,16 @@ var tasks = allPost{
 	},
 }
 
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API Go Redit")
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", indexRoute)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
